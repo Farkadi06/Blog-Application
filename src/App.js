3bd916e2a2f6d00965cf56a340261ff39a199406
FARKADI OUSSAMA <58217000+Farkadi06@users.noreply.github.com>
2021-08-18 11:46:06 +0100

    created the login section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Components/Navbar/Navbar";
 import HomeScreen from "./Screens/HomeScreen/HomeScreen";
+import Login from "./Screens/Login/Login";
 import Settings from "./Screens/Settings/Settings";
 import Single from "./Screens/Single/Single";
 import Write from "./Screens/Write/Write";
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
         <Navbar/>
-        <Settings/>
+        <Login/>
     </div>
   );
 }
---
+++ src/Screens/Login/Login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <span className="loginTitle">Login</span>
+                <form action="" className="loginForm">
+                    <label >Email</label>
+                    <input type="text" className="loginInput" placeholder="Enter your email..." />
+                    <label >Password</label>
+                    <input type="password" className="loginInput"   placeholder="Enter your password..."/>
+                    <button className="loginButton">Login</button>
+                </form>
+            </div>
+                <button className="loginRegisterButton">Register</button>
+        </div>
+    )
+}
+
+export default Login
